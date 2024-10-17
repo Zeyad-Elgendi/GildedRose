7f4d367cd38937bedb5171fabebbe1bb46afc6b1
zeyad  elgendi <121097285+Zeyad-Elgendi@users.noreply.github.com>
2024-10-17 21:44:43 +0300

    Refactored GildedRose.java
    Refactored UpdateItems()
       split it into two major sub functions:
                       Update SellinDate
                      UpdateItemQuality
    
         distributed UpdateItemQuality into 3 sub methods
                                      UpdateLegendaryItem
                                      UpdateBetterWithTimeItems
                                       UpdateTicketItems
     to handle each category's update specifics
     and added guards to handle quality exceeding 50 or falling below 0

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,7 @@
 package com.gildedrose;
 
+import java.util.Objects;
+
 class GildedRose {
     Item[] items;
 
@@ -8,55 +10,69 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+        for( Item i : items){
+            updateSellInDate(i);
+            updateItemQuality(i);
+        }
+    }
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+    private void updateSellInDate(Item item){
+        if(!Objects.equals(item.name, "Sulfuras, Hand of Ragnaros")){
+            item.sellIn --;
+        }
+    }
+    private void updateItemQuality(Item item){
+        switch (item.name){
+            case "Sulfuras, Hand of Ragnaros":
+                break;
+            case "Aged Brie":
+                updateBetterWithTimeItems(item);
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                updateTicketItems(item);
+                break;
+            default:
+                updateNormalItems(item);
+        }
+        if(item.quality >50){
+            item.quality =50;
+        }
+        if(item.quality <0){
+            item.quality =0;
+        }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+    }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+    private void updateBetterWithTimeItems(Item item){
+        if(item.sellIn <=0){
+            item.quality +=2;
+        }else{
+            item.quality ++;
+        }
+    }
+    private void updateTicketItems(Item item){
+        int timeLeft = item.sellIn;
+        if(timeLeft <=0){
+            item.quality =0;
+            return;
+        }
+        if(timeLeft >10){
+            item.quality ++;
+            return;
+        }
+        if( timeLeft >5){
+            item.quality +=2;
+            return;
+        }
+        item.quality +=3;
+
+    }
+    private void updateNormalItems(Item item){
+        if(item.sellIn <=0){
+            item.quality -=2;
+        }else{
+            item.quality -=1;
         }
     }
 }
